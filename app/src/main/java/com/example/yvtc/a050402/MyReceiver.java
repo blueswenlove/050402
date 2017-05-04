c1f30c8401ad13413685f080b051c06191dd3528
blueswen <blueswenlove@gamil.com>
2017-05-04 15:33:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/a050402/MyReceiver.java
@@ -0,0 +1,19 @@
+package com.example.yvtc.a050402;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.d("REC1","接收到了廣播");
+        Intent it = new Intent(context, MyService.class);
+        context.startService(it);
+
+    }
+}
